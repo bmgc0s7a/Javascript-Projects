cdc6bb6eed0f5174fedf87064fd3f98f5cf04ae0
bmgcosta <bmgcosta@outlook.com>
2022-02-25 11:41:39 +0000

    Add action again button and Refactoring code in script.js

--- Guess-Number/script.js
+++ Guess-Number/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberRandom = Math.trunc(Math.random() * 50) + 1;
+let numberRandom = Math.trunc(Math.random() * 50) + 1;
 let attemps = 20;
 let hightScore = 0;
 
@@ -9,36 +9,43 @@ const attempsValue = document.querySelector('.attemps');
 const buttonTry = document.querySelector('.try');
 const numberUser = document.querySelector('.number');
 
+// Function change the textcontent
+const changeContent = function(element, value){
+    document.querySelector(element).textContent = value;
+}
+
+// Action click button AGAIN?
+document.querySelector('.again').addEventListener('click', function(){
+    attemps = 20;
+    numberRandom = Math.trunc(Math.random() * 50) + 1;
+    document.querySelector('body').style.backgroundColor = "";
+    numberUser.disabled = false;
+    buttonTry.hidden = false;
+    notes.textContent = ""; 
+    attempsValue.style.color = "";
+    changeContent('.attemps',String(attemps));  
+})
+
+// CLICK BUTTON TRY
 buttonTry.addEventListener("click", function(){
     const valueInput = Number(numberUser.value);
     if(!valueInput) return;
-    console.log(valueInput, numberRandom);
     if(valueInput === numberRandom){
         document.querySelector('body').style.backgroundColor = "green";
         numberUser.disabled = true;
-        notes.textContent = "Congratulations, You Win";  
-        document.querySelector('.hiddenNumber').textContent = numberRandom;
-        buttonTry.hidden = true;
+        changeContent('.helpnote',"Congratulations, You Win");  
+        changeContent('.hiddenNumber',numberRandom);  
         buttonTry.hidden = true;
         if(hightScore < attemps){ 
             hightScore = attemps;
-            document.querySelector('.score').textContent = hightScore;
+            changeContent('.score',hightScore);
         }
     } else {
-        if(valueInput < numberRandom){
-            notes.textContent = "To low";          
-        } else {
-            notes.textContent = "To high";          
-        }
-        if(attemps > 0){
-            if(attemps < 10){
-                attempsValue.style.color = "orange";
-                if(attemps < 5)
-                attempsValue.style.color = "red";
-            }
-            console.log(attemps);
-            attemps--;
-            attempsValue.textContent = String(attemps);
+        changeContent('.helpnote', (valueInput < numberRandom) ? "To low" : "To High");
+        attemps--;
+        changeContent('.attemps', String(attemps));
+        if(attemps > 0){  
+            attempsValue.style.color = (attemps <= 10) ? (attemps <= 5) ? "red" : "orange" : "";
         } else {
             buttonTry.hidden = true;
             numberUser.disabled = true;
